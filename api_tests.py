da8ab50be08b418c876a99d7ff2d03b30b41ee30
Stefan van den Eertwegh <stefan@formatics.nl>
2015-03-25 11:58:20 +0100

    small check build in, in api_tests.py

--- api_tests.py
+++ api_tests.py
@@ -25,13 +25,17 @@ parameters = {
     'password': password
 }
 resp = requests.post(url='%s/token/' % api_url, data=json.dumps(parameters), headers=headers)
+if 'token' not in resp.json():
+    print "\n=> Please provide the script with an username and password to retrieve the access token!"
+    exit()
+
 access_token = resp.json()['token']
 
 """
 PRINT retrieved access token
 """
 
-print "\nRetrieved access token (KEY): %s\n\n" % access_token
+print "\nRetrieved access token (KEY): %s" % access_token
 
 """
 Loops through calls and makes a GET request to test each API call
@@ -44,5 +48,6 @@ headers = {'Authorization': 'Token %s' % access_token}
 for item in api_calls:
     resp = requests.get(url='%s/%s/' % (api_url, item), headers=headers)
 
-    print '=> list GET call of %s' % item
-    print json.dumps(resp.json(), indent=4, sort_keys=True)
+    print '\n=> Retrieve GET call of %s\n' % item
+    print json.dumps(resp.json())
+    # print json.dumps(resp.json(), indent=4, sort_keys=True)
